2aa857a2687b2afe8762c8d986d1a9364b6fe252
Clinical IT Team <clinical-it@hospital.com>
2026-01-14 12:44:18 +0700

    Fix ImportError: Export filter_topics_by_search from patient_education module

--- components/patient_education/__init__.py
+++ components/patient_education/__init__.py
@@ -4,7 +4,7 @@ Modern, user-friendly components for patient education page
 """
 
 from .cards import render_topic_card, render_topic_grid
-from .search import render_enhanced_search, highlight_search_terms
+from .search import render_enhanced_search, highlight_search_terms, filter_topics_by_search
 from .filters import render_category_filters
 from .viewer import render_enhanced_content, render_table_of_contents, render_reading_progress
 from .related import render_related_topics
@@ -15,6 +15,7 @@ __all__ = [
     'render_topic_grid',
     'render_enhanced_search',
     'highlight_search_terms',
+    'filter_topics_by_search',
     'render_category_filters',
     'render_enhanced_content',
     'render_table_of_contents',
